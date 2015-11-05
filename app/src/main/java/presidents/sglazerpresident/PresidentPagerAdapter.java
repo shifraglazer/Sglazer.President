9ba4d105e49b637741f08afd9e33085197455e32
shifraglazer <shifraglazer@gmail.com>
2015-11-05 14:25:13 -0500

    President info

--- app/src/main/java/presidents/sglazerpresident/PresidentPagerAdapter.java
+++ app/src/main/java/presidents/sglazerpresident/PresidentPagerAdapter.java
@@ -4,7 +4,10 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by student1 on 10/29/2015.
@@ -36,10 +39,19 @@ public class PresidentPagerAdapter extends PagerAdapter {
         View view= inflater.inflate(R.layout.president_pager_item, null);
         TextView name= (TextView) view.findViewById(R.id.name);
         TextView number= (TextView) view.findViewById(R.id.number);
+        TextView life= (TextView) view.findViewById(R.id.life);
+        TextView office= (TextView) view.findViewById(R.id.office);
+        TextView party= (TextView) view.findViewById(R.id.party);
+        ImageView picture= (ImageView) view.findViewById(R.id.picture);
 
         President president= presidents[position];
         name.setText(president.getPresident());
         number.setText(String.valueOf(president.getNumber()));
+        life.setText("President's life: "+president.getBirth_year()+ "-"+ president.getDeath_year());
+        office.setText("Years in office: "+president.getTook_office().substring(0,4)+ "-"+ president.getLeft_office().substring(0,4));
+        party.setText(president.getParty());
+
+        Picasso.with(view.getContext()).load("http://images.tutorcircle.com/cms/images/83/projectile-motion111.PNG").into(picture);
         container.addView(view);
         return view;
     }
